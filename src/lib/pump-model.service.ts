1ee3b85e6c25fb233fccdd736d653eb5e884c09a
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-12-12 13:39:42 +0100

    added a method to fetch model instances

--- src/lib/pump-model.service.ts
+++ src/lib/pump-model.service.ts
@@ -1,4 +1,11 @@
 import {Injectable} from "@angular/core";
+import {HttpClient, HttpContext, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {USE_API_URL} from "common";
+
+// FIXME: set correct url here
+const BIM_API_URL = "bim/";
 
 /** Unused but exported service as example. */
 @Injectable({
@@ -6,4 +13,25 @@ import {Injectable} from "@angular/core";
 })
 export class PumpModelService {
 
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) {}
+
+  fetchModelInstance(
+    modelId: string,
+    instanceId: string
+  ): Observable<object[]> {
+    let url = this.router.parseUrl(BIM_API_URL);
+    url.queryParams["modelID"] = modelId;
+    url.queryParams["instanceID"] = instanceId;
+    return this.http.get(url.toString(), {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      }),
+      responseType: "json",
+      context: new HttpContext()
+        .set(USE_API_URL, true)
+    }) as Observable<object[]>;
+  }
 }
