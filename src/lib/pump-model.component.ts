fb484e54fcf7df50e7eb8a48c58ce84880329523
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-17 17:03:18 +0200

    removed use of old bim component

--- src/lib/pump-model.component.ts
+++ src/lib/pump-model.component.ts
@@ -1,5 +1,4 @@
 import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
-import {BimComponent} from "common";
 
 /** Minimalistic example Component. */
 @Component({
@@ -7,12 +6,4 @@ import {BimComponent} from "common";
 })
 export class PumpModelComponent {
 
-  @ViewChild("bim") bim!: BimComponent;
-  @ViewChild("bimElt") bimElt!: ElementRef<HTMLInputElement>;
-
-  toggleElt(evt: Event) {
-    console.log(this.bimElt.nativeElement.checked);
-    if (this.bimElt.nativeElement.checked) this.bim.show("ELT");
-    else this.bim.hide("ELT");
-  }
 }
