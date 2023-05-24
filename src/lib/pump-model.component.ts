d8e164a52a934fb17736df7068907c050533f48e
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-24 12:18:09 +0200

    added logging for the selected model

--- src/lib/pump-model.component.ts
+++ src/lib/pump-model.component.ts
@@ -1,9 +1,14 @@
 import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {JSONObject} from "web-ifc-three/IFC/BaseDefinitions";
 
 /** Minimalistic example Component. */
 @Component({
   templateUrl: "pump-model.component.html"
 })
 export class PumpModelComponent {
+
+  logModel(model: JSONObject) {
+    console.log(model);
+  }
 
 }
