af0a2a6c8fc08771d744c51c102085f32e5d1fb1
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-24 20:27:13 +0200

    clicked models now show data

--- src/lib/pump-model.component.ts
+++ src/lib/pump-model.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {JSONObject} from "web-ifc-three/IFC/BaseDefinitions";
+import {IfcComponent} from "common";
 
 /** Minimalistic example Component. */
 @Component({
@@ -7,8 +8,32 @@ import {JSONObject} from "web-ifc-three/IFC/BaseDefinitions";
 })
 export class PumpModelComponent {
 
-  logModel(model: JSONObject) {
-    console.log(model);
+  @ViewChild("ifc")
+  viewer!: IfcComponent;
+
+  config: Record<string, IfcComponent.ModelEntry> = {
+    TGA: {path: '/files/WW-Langeoog/20191105-4001110-WW-TGA.ifc', fitToFrame: true, fixed: true},
+    ELT: {path: '/files/WW-Langeoog/20191028-4001110-WW-ELT.ifc'},
+    ARCH: {path: '/files/WW-Langeoog/20191028-4001110-WW-Arch.ifc', visible: false},
+    GEL: {path: '/files/WW-Langeoog/20191028-4001110-WW-Gel.ifc', visible: false}
   }
 
+  data: string = "";
+
+  updateData(model: JSONObject) {
+    console.log(model);
+    this.data = JSON.stringify(model, null, 2);
+  }
+
+  get nonFixedModels() {
+    return Object.entries(this.config).filter(([k, v]) => !v.fixed).map(([k, v]) => k);
+  }
+
+  toggleModel(event: Event, model: string) {
+    let target = event.target as HTMLInputElement;
+    if (target.checked) this.viewer.showModel(model);
+    else this.viewer.hideModel(model);
+  }
+
+  protected readonly InputEvent = InputEvent;
 }
