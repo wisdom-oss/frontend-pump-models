31a5059fc923f08a60a8198ca3aa96c019985b1e
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2023-05-31 13:07:59 +0200

    updated how data is shown

--- src/lib/pump-model.component.ts
+++ src/lib/pump-model.component.ts
@@ -18,10 +18,13 @@ export class PumpModelComponent {
     GEL: {path: '/files/WW-Langeoog/20191028-4001110-WW-Gel.ifc', visible: false}
   }
 
+  showName = "";
   data: string = "";
+
 
   updateData(model: JSONObject) {
     console.log(model);
+    this.showName = model["Name"].value ? model["Name"].value : "";
     this.data = JSON.stringify(model, null, 2);
   }
 
